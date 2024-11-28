ef7d582a381b9489e96a517bce7b78ac82779129
RYN BSD <rynbsd04@gmail.com>
2024-11-28 16:43:55 +0100

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 const ReactCompilerConfig = {
@@ -15,5 +16,43 @@ export default defineConfig({
       },
     }),
     TanStackRouterVite(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        runtimeCaching: [
+          {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            urlPattern: ({ url }) => url.origin === self.origin,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "static-assets",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: "PFE",
+        short_name: "PFE",
+        description: "project fin d'etude",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true, // Enable PWA in development mode
+      },
+    }),
   ],
 });
