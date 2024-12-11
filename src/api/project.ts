904e10bd16ab3f4e3b57c99c5cab3c1c7212c87d
RYN BSD <rynbsd04@gmail.com>
2024-12-11 11:31:33 +0100

    add c/u/d api for project

--- src/api/project.ts
+++ src/api/project.ts
@@ -1,6 +1,6 @@
 import type { FetchResponse } from "../types/http";
 import type { Project, ProjectNote } from "../types/db";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { request } from "../utils/request";
 import { QUERY } from "../constant/query";
 
@@ -24,7 +24,7 @@ export function useGetOne(id: number) {
   return useQuery({
     queryKey: QUERY.PROJECT.ONE(id),
     async queryFn(context) {
-      const res = await request(`/api/project/${id}`, {
+      const res = await request(`/api/project/${encodeURIComponent(id)}`, {
         signal: context.signal,
       });
       const json = (await res.json()) as FetchResponse<FullProject>;
@@ -33,10 +33,61 @@ export function useGetOne(id: number) {
   });
 }
 
+export function useCreate() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn(body: object) {
+      return request(`/api/project`, {
+        method: "POST",
+        body: JSON.stringify(body),
+      });
+    },
+    async onSuccess(res) {
+      if (!res.ok) return;
+      await queryClient.invalidateQueries({ queryKey: QUERY.PROJECT.ALL() });
+    },
+  });
+}
+
+type UseUpdateParams = {
+  id: number;
+  body: object;
+};
+
+export function useUpdate() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn(params: UseUpdateParams) {
+      return request(`/api/project/${encodeURIComponent(params.id)}`, {
+        method: "PUT",
+      });
+    },
+    async onSuccess(res, params) {
+      if (!res.ok) return;
+      await Promise.all([
+        queryClient.invalidateQueries({ queryKey: QUERY.PROJECT.ALL() }),
+        queryClient.invalidateQueries({
+          queryKey: QUERY.PROJECT.ONE(params.id),
+        }),
+      ]);
+    },
+  });
+}
+
 export function useDelete() {
-  return {
-    mutateAsync: async (projectId: number) => {
-      // some async operation
-    }
-  };
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn(id: number) {
+      return request(`/api/project/${encodeURIComponent(id)}`, {
+        method: "DELETE",
+      });
+    },
+    async onSuccess(res, id) {
+      if (!res.ok) return;
+      await Promise.all([
+        queryClient.invalidateQueries({ queryKey: QUERY.PROJECT.ALL() }),
+        queryClient.invalidateQueries({ queryKey: QUERY.PROJECT.ONE(id) }),
+      ]);
+    },
+  });
 }
