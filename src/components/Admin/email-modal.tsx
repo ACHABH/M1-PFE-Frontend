9200ebe1ffc4537dc50174a275f085886a3ef674
RYN BSD <rynbsd04@gmail.com>
2024-12-17 11:43:19 +0100

    complete email

--- src/components/Admin/email-modal.tsx
+++ src/components/Admin/email-modal.tsx
@@ -1,140 +1,18 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import { forwardRef, useEffect } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { useForm } from "../../hooks/useForm";
+import { forwardRef } from "react";
 import AddModal, { type Ref } from "../../layout/add-modal";
-
-
-const TemplateSchema = z.object({
-    id: z.number().min(1),
-    subject: z.string().min(1),
-    content: z.string().min(1),
-  });
-  
-type ZodTemplateSchema = z.infer<typeof TemplateSchema>;
-
-const FormSchema = z.object({
-    id: z.number().min(1),
-    receiver: z.string().min(1),
-    subject: z.string().min(1),
-    content: z.string().min(1),
-});
-  
-type ZodFormSchema = z.infer<typeof FormSchema>;
+import { useGetOne as useGetOneEmail } from "../../api/email";
 
 type Props = {
-    email: ZodFormSchema;
-    template: ZodTemplateSchema[];
-    isEdit: boolean;
-    onClose: () => void;
+  emailId: number;
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
-export default forwardRef<Ref, Props>(({ email, template , onClose, isEdit }, ref) => {
-    const form = useForm<ZodFormSchema>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            id: email?.id || 0,
-            receiver: email?.receiver || "",
-            subject: email?.subject || (isEdit ? "" : template[0]?.subject || ""),
-            content: email?.content || (isEdit ? "" : template[0]?.content || ""),
-        },
-        values: {
-            id: email.id ?? "",
-            receiver: email.receiver ?? "",
-            subject: email.subject ?? "",
-            content: email.content ?? "",
-        },
-    });
+export default forwardRef<Ref, Props>(({ emailId }, ref) => {
+  const { data: email } = useGetOneEmail(emailId);
 
-    const templateForm = useForm<ZodTemplateSchema>({
-        resolver: zodResolver(TemplateSchema),
-        defaultValues: {
-          id: template[0]?.id ?? "",
-          subject: template[0]?.subject ?? "",
-          content: template[0]?.content ?? "",
-        },
-        values: {
-          id: template[0]?.id ?? "",
-          subject: template[0]?.subject ?? "",
-          content: template[0]?.content ?? "",
-        },
-    });
-
-    const selectedTemplateId = templateForm.watch("id");
-
-    const selectedTemplate = template.find(
-        (t) => t.id === Number(selectedTemplateId)
-    );
-    useEffect(() => {
-        if (selectedTemplate) {
-            form.setValue("subject", selectedTemplate.subject);
-            form.setValue("content", selectedTemplate.content);
-        } else {
-            templateForm.setValue("subject", "");
-            templateForm.setValue("content", "");
-        }
-    }, [selectedTemplate, form, templateForm]);
-
-    return (
-        <AddModal ref={ref} title={email ? "Update Schedule" : "Add New Schedule"} action={null}>
-            <Form onSubmit={() => console.log(email)}>
-            <div className="d-flex flex-wrap justify-content-between">
-                <Form.Group className="mb-3" style={{ maxWidth: "47%" }}>
-                    <Form.Label>Template (Optional)</Form.Label>
-                    <Form.Select aria-label="Default select example" {...templateForm.register("id")} name="id">
-                    <option value="">-- Select a Template --</option>
-                    {template.map((t) => (
-                    <option key={t.id} value={t.id}>
-                        {t.subject}
-                    </option>
-                    ))}
-                    </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-3" style={{ width: "47%" }}>
-                <Form.Label>Receiver</Form.Label>
-                    <Form.Select
-                    aria-label="Receiver select"
-                    {...form.register("receiver", { required: true })}
-                    name="receiver"
-                    required
-                    >
-                    <option value="">-- Select Receiver --</option>
-                    {["All", "Students", "Teachers", "Companies"].map((receiver) => (
-                    <option key={receiver} value={receiver}>
-                    {receiver}
-                    </option>
-                    ))}
-                    </Form.Select>
-                </Form.Group>
-            </div>
-            <Form.Group className="mb-3">
-            <Form.Label>Subject</Form.Label>
-            <Form.Control
-                {...form.register("subject", { required: true })}
-                type="text"
-                readOnly={!!selectedTemplate}
-            />
-            </Form.Group>
-            <Form.Group className="mb-3">
-            <Form.Label>Content</Form.Label>
-            <Form.Control
-                {...form.register("content", { required: true })}
-                as="textarea"
-                name="description"
-                rows={4}
-                readOnly={!!selectedTemplate}
-            />
-            </Form.Group>
-            <Button type="submit" className="me-2" variant="success">
-                {email ? "Update" : "Add"}
-            </Button>
-            <Button type="reset" variant="secondary" onClick={onClose}>
-                Cancel
-            </Button>
-            </Form>
-        </AddModal>
-    );
+  return (
+    <AddModal ref={ref} title={email?.subject ?? ""} action={null}>
+      <pre>{JSON.stringify(email, null, 2)}</pre>
+    </AddModal>
+  );
 });
--- src/routes/dashboard/admin/emails.lazy.tsx
+++ src/routes/dashboard/admin/emails.lazy.tsx
@@ -3,15 +3,18 @@ import type { Email } from "../../../types/db";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import { type ElementRef, useMemo, useRef, useState } from "react";
 import { useGetAll as useGetAllEmails } from "../../../api/email";
-import { useMemo, useState } from "react";
 import Table from "../../../components/table";
+import EmailModal from "../../../components/admin/email-modal";
 
 export const Route = createLazyFileRoute("/dashboard/admin/emails")({
   component: Component,
 });
 
 function Component() {
+  const ref = useRef<ElementRef<typeof EmailModal>>(null);
+
   const { data: emails } = useGetAllEmails();
 
   const [emailId, setEmailId] = useState<number | null>(null);
@@ -43,6 +46,7 @@ function Component() {
               size="sm"
               onClick={() => {
                 setEmailId(emailId);
+                ref.current?.show();
               }}
             >
               View
@@ -57,6 +61,7 @@ function Component() {
     <Container as="div" className="mt-4">
       <h1>Emails</h1>
       <Table data={emails} columns={columns} />
+      {emailId && <EmailModal ref={ref} emailId={emailId} />}
     </Container>
   );
 }
