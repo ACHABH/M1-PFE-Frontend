99bcb38adbeaad97ebf85b8c3b2aa7e4235a7a4b
RYN BSD <rynbsd04@gmail.com>
2025-01-17 15:41:19 +0100

    config

---
+++ src/api/sql.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { request } from "../utils/request";
+
+export type Query = string;
+
+export type QueryType = "select" | "insert" | "update" | "delete";
+
+export async function sql(type: QueryType, query: Query) {
+  const res = await request("/api/sql", {
+    method: "POST",
+    body: JSON.stringify({
+      type,
+      query,
+    }),
+  });
+  const json = await res.json();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return json as any;
+}
+
+export function useSelectSql(query: Query) {
+  return useQuery({
+    queryKey: [`${Date.now() * Math.random()}`],
+    queryFn: async () => {
+      return sql("select", query);
+    },
+  });
+}
