6e45ba5ee8469d25c40f1de7f343ce48701789d2
Yacine205 <zinoyacine56@gmail.com>
2025-01-20 20:38:28 +0100

    propose form problem

--- src/routes/dashboard/student/homepage.lazy.tsx
+++ src/routes/dashboard/student/homepage.lazy.tsx
@@ -1,31 +1,53 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { Badge, Button, Card, Col, Row } from "react-bootstrap";
+import { Badge, Card, Col, Row } from "react-bootstrap";
+import { useProfile } from "../../../api/user";
+import { useSelectSql } from "../../../api/sql";
 
 export const Route = createLazyFileRoute("/dashboard/student/homepage")({
   component: Component,
 });
 
-const profile = {
-  name: "Houari Yacine",
-  initials: "HY",
-  specialty: "GL",
-  average: "16.9",
-};
-
-const assignedProject = {
-  title: "AI-Based Health Diagnosis",
-  description:
-    "Using AI to predict and diagnose diseases based on health data.",
-  timeLeft: "2025-6-08",
-  type: "1275",
-  status: "In Progress",
-  supervisor: "Dr. Sarah Johnson",
-};
+// const assignedProject = {
+//   title: "AI-Based Health Diagnosis",
+//   description:
+//     "Using AI to predict and diagnose diseases based on health data",
+//   timeLeft: "2025-6-08",
+//   type: "1275",
+//   status: "In Progress",
+//   supervisor: "Dr. Sarah Johnson",
+// };
 
 function Component() {
+  const {
+    data: user,
+    isLoading: isUserLoading,
+    isError: isUserError,
+    error: userError,
+  } = useProfile();
+
+  const {
+    data: studentData,
+    isLoading: isStudentLoading,
+    isError: isStudentError,
+    error: studentError,
+  } = useSelectSql(`SELECT * FROM students WHERE user_id = ${user?.id};`);
+
+  const {
+    data: assignedProjectData
+  } = useSelectSql(
+    `SELECT * FROM projects WHERE status = 'assigned';`
+  );
+
+  const assignedProject = assignedProjectData?.data?.[0];
+  const student = studentData?.data?.[0];
+
+  if (isUserLoading || isStudentLoading) return <div>Loading...</div>;
+  if (isUserError) return <div>Error: {userError.message}</div>;
+  if (isStudentError) return <div>Error: {studentError.message}</div>;
+
   return (
     <div>
-      <div className="mb-5">
+      <div className="mt-5 mb-5">
         <h2>Welcome to the Homepage</h2>
       </div>
       <Row className="mt-4">
@@ -39,20 +61,15 @@ function Component() {
                   className="rounded-circle bg-primary text-white d-flex align-items-center justify-content-center me-3"
                   style={{ width: "50px", height: "50px" }}
                 >
-                  {profile.initials}
+                  {user?.last_name?.charAt(0).toUpperCase()}
+                  {user?.first_name?.charAt(0).toUpperCase()}
                 </div>
                 {/* Welcome Message */}
                 <div className="text-start">
-                  <Card.Title>{profile.name}</Card.Title>
-                  <Button
-                    variant="link"
-                    className="p-0"
-                    onClick={() => {
-                      console.log("Logout clicked");
-                    }}
-                  >
-                    Sign out
-                  </Button>
+                  <Card.Title>
+                    {user?.last_name} {user?.first_name}
+                  </Card.Title>
+
                 </div>
               </div>
             </Card.Body>
@@ -66,42 +83,41 @@ function Component() {
                 Profile Overview
               </Card.Title>
               <p className="mb-2">
-                <strong>Specialty:</strong> {profile.specialty}
+                <strong>Specialty:</strong> {student?.major}
               </p>
               <p>
-                <strong>Average:</strong> {profile.average}
+                <strong>Average:</strong> {student?.average_score}
               </p>
             </Card.Body>
           </Card>
         </Col>
       </Row>
-      <div className="mt-2">
-        {/* <h3>Your Assigned Project</h3> */}
-        <h3>Your Assigned Project</h3>
-        <p className="mb-4">
-          Here is your assigned project.
-        </p>
-        <Card className="mt-3">
-          <Card.Body>
-            <Card.Title>{assignedProject.title}</Card.Title>
-            <Badge
-              bg={assignedProject.type === "1275" ? "success" : "secondary"}
-            >
-              {assignedProject.type}
-            </Badge>
-            <p className="mt-3">{assignedProject.description}</p>
-            <p>
-              <strong>Status:</strong> {assignedProject.status}
-            </p>
-            <p>
-              <strong>Supervisor:</strong> {assignedProject.supervisor}
-            </p>
-            <p className="text-danger">
-              <strong>Time Left:</strong> {assignedProject.timeLeft}
-            </p>
-          </Card.Body>
-        </Card>
-      </div>
+      {assignedProject && (
+        <div className="mt-2">
+          <h3>Your Assigned Project</h3>
+          <p className="mb-4">Here is your assigned project.</p>
+          <Card className="mt-3">
+            <Card.Body>
+              <Card.Title>{assignedProject.title}</Card.Title>
+              <Badge
+                bg={assignedProject.type === "1275" ? "success" : "secondary"}
+              >
+                {assignedProject.type}
+              </Badge>
+              <p className="mt-3">{assignedProject.description}</p>
+              <p>
+                <strong>Status:</strong> {assignedProject.status}
+              </p>
+              {/* <p>
+                <strong>Supervisor:</strong> {assignedProject.supervisor}
+              </p>
+              <p className="text-danger">
+                <strong>Time Left:</strong> {assignedProject.timeLeft}
+              </p> */}
+            </Card.Body>
+          </Card>
+        </div>
+      )}
     </div>
   );
 }
--- src/routes/dashboard/student/propose-topic.lazy.tsx
+++ src/routes/dashboard/student/propose-topic.lazy.tsx
@@ -1,43 +1,171 @@
+import { useState, useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useAuth } from "../../../api/auth";
+import { sql } from "../../../api/sql.ts";
+import { STUDENT_MAJOR } from "../../../constant/enum";
 
 export const Route = createLazyFileRoute("/dashboard/student/propose-topic")({
   component: Component,
 });
 
-interface FormData {
-  studentFirstName: string;
-  studentLastName: string;
-  binomeFirstName?: string;
-  binomeLastName?: string;
-  specialty: string;
-  title: string;
-  description: string;
-  technologies: string;
-  materials: string;
-}
+// Zod schema for form validation
+const FormSchema = z.object({
+  studentFirstName: z.string().trim().min(1, "First name is required"),
+  studentLastName: z.string().trim().min(1, "Last name is required"),
+  binomeFirstName: z.string().trim().optional(),
+  binomeLastName: z.string().trim().optional(),
+  specialty: z.string().trim().min(1, "Specialty is required"),
+  title: z.string().trim().min(1, "Title is required"),
+  description: z.string().trim().min(1, "Description is required"),
+  technologies: z.string().trim().min(1, "Technologies are required"),
+  materials: z.string().trim().min(1, "Materials are required"),
+});
+
+type ZodFormSchema = z.infer<typeof FormSchema>;
 
 function Component() {
+  const user = useAuth((user) => {
+    if (user) return;
+  });
+
   const {
     control,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<ZodFormSchema>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      studentFirstName: user?.first_name,
+      studentLastName: user?.last_name,
+      binomeFirstName: "",
+      binomeLastName: "",
+      specialty: "",
+      title: "",
+      description: "",
+      technologies: "",
+      materials: "",
+    },
+  });
 
-  const onSubmit = (data: FormData) => {
-    console.log("Proposed Topic Data:", data);
-    reset();
+  const [submissionReminder, setSubmissionReminder] = useState(false);
+
+  // Simulated Reminder Logic
+  useEffect(() => {
+    const deadline = new Date("2025-12-30"); // Proposal deadline
+    const now = new Date();
+    if (now < deadline) {
+      setSubmissionReminder(true);
+    }
+  }, []);
+
+  async function getBinomeId(data: ZodFormSchema) {
+    if (!data.binomeFirstName || !data.binomeLastName) return null;
+
+    const binomeQuery = `
+      SELECT id FROM users 
+      WHERE first_name = '${data.binomeFirstName}' 
+      AND last_name = '${data.binomeLastName}' 
+      LIMIT 1;
+    `;
+    const binomeResult = await sql("select", binomeQuery);
+    const binomeId = binomeResult.data[0]?.id;
+    if (!binomeId) {
+      alert("Binome not found.");
+      throw new Error("Binome not found.");
+    }
+    return binomeId;
+  }
+
+  async function createGroup(studentId: number, binomeId: number | null) {
+    const insertGroupQuery = `
+      INSERT INTO groups DEFAULT VALUES;
+    `;
+    const groupResult = await sql("insert", insertGroupQuery);
+
+    const groupId = groupResult.data.last_insert_rowid;
+
+    const insertStudentQuery = `
+      INSERT INTO group_members (student_id, group_id)
+      VALUES (${studentId}, ${groupId});
+    `;
+    await sql("insert", insertStudentQuery);
+
+    if (binomeId) {
+      const insertBinomeQuery = `
+        INSERT INTO group_members (student_id, group_id)
+        VALUES (${binomeId}, ${groupId});
+      `;
+      await sql("insert", insertBinomeQuery);
+    }
+    return groupId;
+  }
+
+  async function getProjectId() {
+    const lastProjectIdQuery = `SELECT COUNT(*) as count FROM projects;`;
+    const lastProjectIdResult = await sql("select", lastProjectIdQuery);
+    const count = lastProjectIdResult.data[0]?.count;
+    const lastProjectId = count ? parseInt(count, 10) + 1 : 1;
+    return lastProjectId;
+  }
+
+  const onSubmit = async (data: ZodFormSchema) => {
+    try {
+      const lastProjectId = await getProjectId();
+      const formattedDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+      const query = `
+        INSERT INTO projects (title, description, type, status, technologies, material_needs, master_option, created_at)
+        VALUES (
+          '${data.title.replace(/'/g, "''")}', 
+          '${data.description.replace(/'/g, "''")}', 
+          'STUDENT_PROPOSAL', 
+          'PROPOSED', 
+          '${data.technologies.replace(/'/g, "''")}', 
+          '${data.materials.replace(/'/g, "''")}', 
+          '${data.specialty.toUpperCase().replace(/'/g, "''")}',
+          '${formattedDate}'
+        )
+      `;
+
+      const query2 = `
+        INSERT INTO project_propositions 
+        (user_id, project_id, status, validated_by)
+        VALUES (
+          ${user?.id},
+          ${lastProjectId},
+          'pending',
+          1
+        )
+      `;
+
+      const result = await sql("insert", query);
+      const result2 = await sql("insert", query2);
+      const result3 = await createGroup( user?.id , getBinomeId(data));
+      
+      console.log("Proposal submitted:", result);
+      console.log("Proposal proposition:", result2);
+      console.log("Proposal proposition:", result3);
+      alert("Proposal submitted successfully!");
+      reset();
+    } catch (error) {
+      console.error("Error submitting proposal:", error);
+      alert("Failed to submit the proposal. Please try again.");
+    }
   };
 
   return (
-    <Container
-      className="my-3 component-bg rounded p-3"
-      style={{ width: "90%" }}
-    >
+    <Container className="my-3 component-bg rounded p-3" style={{ width: "90%" }}>
       <h3>Submit Your Topic Proposal</h3>
-
+      {submissionReminder && (
+        <div className="alert alert-warning" role="alert">
+          <strong>Reminder:</strong> The deadline for submitting PFE proposals is December 30, 2024.
+        </div>
+      )}
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Row className="mb-3">
           <Col md={6}>
@@ -47,7 +175,6 @@ function Component() {
                 name="studentFirstName"
                 control={control}
                 defaultValue=""
-                rules={{ required: "First name is required" }}
                 render={({ field }) => (
                   <Form.Control
                     {...field}
@@ -68,7 +195,6 @@ function Component() {
                 name="studentLastName"
                 control={control}
                 defaultValue=""
-                rules={{ required: "Last name is required" }}
                 render={({ field }) => (
                   <Form.Control
                     {...field}
@@ -127,13 +253,15 @@ function Component() {
                 name="specialty"
                 control={control}
                 defaultValue=""
-                rules={{ required: "Specialty is required" }}
                 render={({ field }) => (
-                  <Form.Control
-                    {...field}
-                    placeholder="Enter your specialty"
-                    isInvalid={!!errors.specialty}
-                  />
+                  <Form.Select {...field} isInvalid={!!errors.specialty}>
+                    <option value="">Select your specialty</option>
+                    {STUDENT_MAJOR.map((major) => (
+                      <option key={major} value={major}>
+                        {major.toUpperCase()}
+                      </option>
+                    ))}
+                  </Form.Select>
                 )}
               />
               <Form.Control.Feedback type="invalid">
@@ -148,7 +276,6 @@ function Component() {
                 name="title"
                 control={control}
                 defaultValue=""
-                rules={{ required: "Title is required" }}
                 render={({ field }) => (
                   <Form.Control
                     {...field}
@@ -166,7 +293,7 @@ function Component() {
 
         <Row className="mb-3">
           <Col md={6}>
-            <Form.Group controlId="technologies" className="mb-3">
+            <Form.Group controlId="technologies">
               <Form.Label>Technologies</Form.Label>
               <Controller
                 name="technologies"
@@ -176,10 +303,16 @@ function Component() {
                   <Form.Control
                     {...field}
                     placeholder="Enter technologies to be used"
+                    isInvalid={!!errors.technologies}
                   />
                 )}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors.technologies?.message}
+              </Form.Control.Feedback>
             </Form.Group>
+          </Col>
+          <Col md={6}>
             <Form.Group controlId="materials">
               <Form.Label>Materials</Form.Label>
               <Controller
@@ -192,20 +325,25 @@ function Component() {
                     as="textarea"
                     rows={3}
                     placeholder="Enter any materials needed for the project"
+                    isInvalid={!!errors.materials}
                   />
                 )}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors.materials?.message}
+              </Form.Control.Feedback>
             </Form.Group>
           </Col>
+        </Row>
 
-          <Col md={6}>
+        <Row className="mb-3">
+          <Col md={12}>
             <Form.Group controlId="description">
               <Form.Label>Description</Form.Label>
               <Controller
                 name="description"
                 control={control}
                 defaultValue=""
-                rules={{ required: "Description is required" }}
                 render={({ field }) => (
                   <Form.Control
                     {...field}
